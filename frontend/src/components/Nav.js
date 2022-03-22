8df2d57f258ab14f161507c335f9752029b64265
Oren Saldanha <orensaldanha@gmail.com>
2022-03-22 21:14:29 +0530

    Modified search bar

--- frontend/src/components/Nav.js
+++ frontend/src/components/Nav.js
@@ -5,67 +5,50 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Dropdown}from 'react-bootstrap'
 import {LoginContext} from './Context'
 import { useNavigate } from 'react-router-dom'
-
+import SearchBar from './SearchBar'
 
 const Nav = () => {
     const {loggedIn,setloggedIn}=useContext(LoginContext)
-
     const first_name=sessionStorage.getItem('first_name')
     let navigate=useNavigate();
-
-
     const logout=()=>{
         sessionStorage.removeItem('token')
         sessionStorage.removeItem('first_name')
         sessionStorage.removeItem('storedLoggedIn')
         setloggedIn(false)
         navigate('/login')
+
     }
-
-    const [search, setSearch] = React.useState('');
-
-    function handleSubmit(event) {
-        event.preventDefault();
-        navigate('/search-results?query=' + search)
-    }
-
-    const searchStyle = {
- 
-      };
-
-
     return (
-        <div>
+        <div className='nav-div'>
             <nav className='nav-bar'>
-                <div>
+                    <div>
+                        <div className='logo-cntr'>
                     <Link to='/'>
-                        <img className='logo-image' src={logoimg} alt=' '></img>
+                        <img className='logo-image'src={logoimg} alt=' '></img>
                     </Link>
-                    {loggedIn ? (
-                            <Dropdown className='profile-btn'>
-                                <Dropdown.Toggle variant="primary" id="dropdown-basic">
-                                    Hi,{first_name}
-                                </Dropdown.Toggle>
-
-                                <Dropdown.Menu>
-                                    <form className={searchStyle} onSubmit={handleSubmit}>
-                                        <input className="form-control" type="search" placeholder="Search" aria-label="Search"
-                                            id="search"
-                                            value={search}
-                                            onChange={(e) => setSearch(e.target.value)}
-                                        />
-                                    </form>
-                                    <Dropdown.Item href="/Archive">Archive</Dropdown.Item>
-                                    <Dropdown.Item as="button" onClick={logout}>Logout</Dropdown.Item>
-                                </Dropdown.Menu>
-                            </Dropdown>
-                                ):(<></>)
-                }
-
                     </div>
-                </nav>
+
+
+
+                    {loggedIn ? 
+                (<div className='nav-search-drop'>
+                     <SearchBar/>
+                    <Dropdown className='profile-btn'>
+                        <Dropdown.Toggle variant="primary" id="dropdown-basic">
+                            Hi,{first_name}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="/Archive">Archive</Dropdown.Item>
+                            <Dropdown.Item as="button" onClick={logout}>Logout</Dropdown.Item>
+                        </Dropdown.Menu>
+                        </Dropdown>
+                        </div>):(<></>)}
+
+                                         
+                    </div>
+            </nav>
         </div>
     )
 }
-
 export default Nav
